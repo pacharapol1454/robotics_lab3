a27efb70bc89f7e7de3413d487ed61ed91919140
pacharapol1454 <61011454@kmitl.ac.th>
2020-02-04 16:39:05 +0700

    third_commit

--- week2.py
+++ week2.py
@@ -1,17 +1,18 @@
-c=0
-psw = input()
-for i in psw:
- if i.isalpha():
-  c=c+1
-if c==0:
- psw_int = int(psw)
- digit = 0
- while(psw_int > 0):
-  psw_int = psw_int//10
-  digit = digit + 1
- if digit == 4 or digit == 6:
-  print("true")
+def validate_pin(psw):
+ c=0
+ state=0
+ for i in psw:
+  if i.isalpha():
+   c=c+1
+ if c==0:
+  psw_int = int(psw)
+  digit = 0
+  while(psw_int > 0):
+   psw_int = psw_int//10
+   digit = digit + 1
+  if digit == 4 or digit == 6:
+   c=c+1
+ if state==1:
+     return True
  else:
-  print("false")
-else:
- print("false")
+     return False
